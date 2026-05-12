57e99bd7209190f8b788faf35c0d5ad06020f909
im25-pawlowskiv <pawlowskiv@bzz.ch>
2026-05-12 09:11:12 +0200

    Classes Created

--- model.py
+++ model.py
@@ -1,12 +1,73 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, ForeignKey, LargeBinary
 from main import engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
+class Material(Base):
+    __tablename__ = "Material"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    email = Column(String(100))
+    MaterialID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    Material = Column(String(100), nullable=False)
+    IsAktiv = Column(Boolean, nullable=False, default=True)
+
+class Kategorie(Base):
+    __tablename__ = "Kategorie"
+
+    KategorieID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    Kategorie = Column(String(100), nullable=False)
+    IsAktiv = Column(Boolean, nullable=False, default=True)
+
+class Prioritaet(Base):
+    __tablename__ = "Prioritaet"
+
+    PrioritaetID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    Prioritaet = Column(String(100), nullable=False)
+
+class Fortschritt(Base):
+    __tablename__ = "Fortschritt"
+
+    FortschrittID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    Fortschritt = Column(String(100), nullable=False)
+
+class Benutzer(Base):
+    __tablename__ = "Benutzer"
+
+    BenutzerID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    BenutzerName = Column(String(100), nullable=False)
+    BenutzerPWD = Column(String(100), nullable=False)
+
+class Aufgabe(Base):
+    __tablename__ = "Aufgabe"
+
+    AufgabeID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    Titel = Column(String(100), nullable=False)
+    Ende = Column(DateTime, nullable=True)
+    Ort = Column(String(250), nullable=True)
+    Koordinaten = Column(String(100), nullable=True)
+    Notiz = Column(Text(100), nullable=True)
+    KategorieID = Column(Integer,ForeignKey("Kategorie.KategorieID"), nullable=False)
+    PrioritaetID = Column(Integer,ForeignKey("Prioritaet.PrioritaetID"), nullable=False)
+    FortschrittID = Column(Integer, ForeignKey("Fortschritt.FortschrittID"), nullable=False)
+    BenutzerID = Column(Integer, ForeignKey("Benutzer.BenutzerID"), nullable=False)
+
+    kategorie = relationship("Kategorie")
+    prioritaet = relationship("Prioritaet")
+    fortschritt = relationship("Fortschritt")
+    benutzer = relationship("Benutzer")
+
+class Datei(Base):
+    __tablename__ = "Datei"
+
+    DateiID = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    AufgabeID = Column(Integer, ForeignKey("Aufgabe.AufgabeID"), nullable=False)
+    Dateipfad = Column(String(250))
+    DateiBLOB = Column(LargeBinary)
+
+class AufgabeMaterial(Base):
+    __tablename__ = "AufgabeMaterial"
+
+    AufgabeID = Column(Integer, ForeignKey("Aufgabe.AufgabeID"), primary_key=True, nullable=False)
+    MaterialID = Column(Integer, ForeignKey("Material.MaterialID"), primary_key=True, nullable=False)
+    Anzahl = Column(Integer, nullable=True)
+
